1e022728eed59b8ffcff0ece0e63c52abcf6e7a7
RAJAKUMAR786786 <98510859+RAJAKUMAR786786@users.noreply.github.com>
2022-01-26 23:12:21 -1200

    initial commit

---
+++ Employee1.java
@@ -0,0 +1,16 @@
+package org.emp1;
+
+public class Employee1 extends Client {
+public void empName(){
+System.out.println("employee name is RAJA");	
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Employee1 emp1=new Employee1();
+emp1.empName();
+emp1.ClientName();
+emp1.projectName();
+emp1.companyName();
+	}
+
+}
