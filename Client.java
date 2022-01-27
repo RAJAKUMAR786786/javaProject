60cd6e54d8dd731407330a6c3646a24ca774e8bc
RAJAKUMAR786786 <98510859+RAJAKUMAR786786@users.noreply.github.com>
2022-01-27 02:44:46 -1200

    initial commit

---
+++ Client.java
@@ -0,0 +1,13 @@
+package emp.orga;
+
+public class Client {
+	public void ClientName(){
+		System.out.println("client name is sekar");	
+		}
+		
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
